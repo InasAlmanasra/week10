0a2b667f75357e03a8b8de27866162208e2c07e5
Inas Almanasra <i.almanasra.17@ucl.ac.uk>
2018-04-12 08:22:26 -0400

    coddee

--- ucesihi/www/httpServer.js
+++ ucesihi/www/httpServer.js
@@ -22,7 +22,7 @@ app.post('/uploadData',function(req,res){
 	// note that we are using POST here as we are uploading data
 	// so the parameters form part of the BODY of the request rather than the RESTful API
 	console.dir(req.body);
-    
+
  	pool.connect(function(err,client,done) {
        	if(err){
           	console.log("not able to get connection "+ err);
@@ -33,9 +33,9 @@ app.post('/uploadData',function(req,res){
 // well known text should look like: 'POINT(-71.064544 42.28787)'
 var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")'";
 
-var querystring = "INSERT into formquiz (geom,question,opta,optb,optc,optd,opte,correct) values ('";
-querystring = querystring + geometrystring + "))";
-querystring = querystring +req.body.question + "','" +req.body.opta + "','" +req.body.optb + "','" +req.body.optc + "','" +req.body.optd + "','" +req.body.opte + "','" +req.body.correct + "','" ;
+var querystring = "INSERT into formdata (name,surname,module,language, modulelist, lecturetime, geom) values ('";
+querystring = querystring + req.body.name + "','" + req.body.surname + "','" + req.body.module + "','";
+querystring = querystring + req.body.language + "','" + req.body.modulelist + "','" + req.body.lecturetime+"',"+geometrystring + "))";
        	console.log(querystring);
        	client.query( querystring,function(err,result) {
           done(); 
